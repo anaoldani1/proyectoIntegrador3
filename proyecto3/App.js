1fc5e775d7cd3daa80805314fb6949034b48e4af
anaoldani1 <oldania@udesa.edu.ar>
2025-11-03 16:41:16 -0300

    register

--- proyecto3/App.js
+++ proyecto3/App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 import Login from './src/screens/Login';
 import Register from './src/screens/Register';
 import HomeMenu from './src/components/HomeMenu';
